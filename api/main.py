7f984b9001ae2d44c5cbcad5664e7bc122d897c4
backy <happy.backy@gmail.com>
2024-05-21 17:06:02 +0900

    fix host

--- api/main.py
+++ api/main.py
@@ -42,4 +42,4 @@ def init_app():
 app = init_app()
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="localhost", port=8888, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
